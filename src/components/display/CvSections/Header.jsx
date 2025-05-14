9f59b294415128533ac5ee16d17b3833053aa313
Colland <t.jack.mac@gmail.com>
2025-05-14 19:59:54 +1200

    Added icons to CV personal details in header

--- src/components/display/CvSections/Header.jsx
+++ src/components/display/CvSections/Header.jsx
@@ -1,11 +1,21 @@
+import { MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+import { FaHouse } from "react-icons/fa6";
+
 function Header({ personalDetails }) {
   return (
     <div className="w-full h-1/8 bg-[#39415d] flex flex-col justify-center items-center gap-4">
       <h1 className="text-gray-100 text-3xl font-bold">{personalDetails.fullName}</h1>
       <div className="flex text-gray-100 text-[17px] font-header gap-8">
-        <p>{personalDetails.email}</p>
-        <p>{personalDetails.phoneNum}</p>
-        <p>{personalDetails.address}</p>
+        <p className="flex items-center">
+          {personalDetails.email !== "" && <MdEmail className="mr-2" />}{personalDetails.email}
+        </p>
+        <p className="flex items-center">
+          {personalDetails.phoneNum !== "" && <FaPhone className="mr-2" />}{personalDetails.phoneNum}
+        </p>
+        <p className="flex items-center">
+          {personalDetails.address !== "" && <FaHouse className="mr-2" />}{personalDetails.address}
+        </p>
       </div>
     </div>
   )
